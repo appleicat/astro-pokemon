f831a8551710364c65438dffca7967c7fb87a010
appleicat <appleicat@outlook.com>
2025-02-26 22:33:12 +0400

    add wyw-in-js vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import linaria from "@wyw-in-js/vite";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react({ include: ["**/react/*"] })],
+  vite: { plugins: [linaria()] },
 });
