a28ae5c4ba13d9c98bab2c89448b3cad0d4a10a5
appleicat <appleicat@outlook.com>
2025-02-28 00:17:06 +0400

    add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,6 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [react({ include: ["**/react/*"] })],
   vite: { plugins: [linaria()] },
+  site: "https://appleicat.github.io",
+  base: "/astro-pokemon",
 });
